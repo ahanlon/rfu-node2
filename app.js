3ac059c352c9caf60cb60f68d77732722e300e83
ahanlon <hanlonad27@gmail.com>
2015-07-21 15:51:58 -0600

    finished bonus

--- app.js
+++ app.js
@@ -10,17 +10,31 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
 
+// ------------------- Bonus ----------------------------
 
-app.get('/', function(req, res){ // this handles the get request
- 
-		fs.readFile('data.txt', function(err, data){ // this reads the data.txt file
+app.get('/:page', function(req, res){
+
+	req.params.page;
+	fs.readFile('/:page', function(err, data){ // this reads the data.txt file
 	    res.header('Content-Type', 'text/html'); // this tells the server to look for text
 		res.send(data);
 	});
-	
+
 });
 
-app.get('/', indexController.index);
+
+// --------------- Part 3 making it asynchronus  ---------------------
+
+// app.get('/', function(req, res){ // this handles the get request
+ 
+// 		fs.readFile('data.txt', function(err, data){ // this reads the data.txt file
+// 	    res.header('Content-Type', 'text/html'); // this tells the server to look for text
+// 		res.send(data);
+// 	});
+	
+// });
+
+// app.get('/', indexController.index);
 
 var server = app.listen(7935, function(req, res) {
 	console.log('Express server listening on port ' + server.address().port);
